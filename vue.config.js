a6a60bb38d141b2ddb233c4a9d71930b2bfdc2a3
perfectSymphony <2066309334@qq.com>
2019-04-26 18:25:14 +0800

    配置vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+'use strict'
+const path = require('path')
+
+
+const port = 8888
+
+//所有的详细配置都在 https://cli.vuejs.org/config/
+module.exports = {
+    publicPath: '/',
+    outputDir: 'dist',
+    assetDir: 'static',
+
+}
