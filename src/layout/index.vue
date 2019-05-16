4a4ecc1655c215e47b447e9111cdbb70f1105ae7
perfectSymphony <2066309334@qq.com>
2019-05-16 19:25:32 +0800

    layout布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,77 @@
 <template>
-    <div>123</div>
+    <div class="app-wrapper">
+        <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>        
+            <!--  @open="handleOpen" @close="handleClose" :collapse="isCollapse" -->
+        <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+            <el-menu default-active="1-4-1" class="el-menu-vertical-demo">
+            <el-submenu index="1">
+                <template slot="title">
+                <i class="el-icon-location"></i>
+                <span slot="title">导航一</span>
+                </template>
+                <el-menu-item-group>
+                <span slot="title">分组一</span>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                <span slot="title">选项4</span>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+            </el-menu>    
+        </el-radio-group> -->
+    </div>
 </template>
 <script>
-export default {
-    
-}
+  export default {
+    name: 'Layout',
+    data() {
+      return {
+        isCollapse: true
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
 </script>
 <style lang="scss" scoped>
+   @import "~@/styles/mixin.scss";
 
+   .app-wrapper {
+       position: relative;
+       width: 100%;
+       height:100%;
+   }
+
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
 </style>
