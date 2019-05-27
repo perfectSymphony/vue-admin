64208ac10aea8409e88874a026953281cf502bdc
perfectSymphony <2066309334@qq.com>
2019-05-27 18:13:06 +0800

    todos

---
+++ src/views/dashboard/admin/components/TodoList/Todo.vue
@@ -0,0 +1,45 @@
+<template>
+    <li class="todo">
+        <div class="view">
+            <input type="checkbox" class="toggle">
+            <label for="">star this repository</label>
+            <button class="destroy">
+
+            </button>
+        </div>
+        <input type="text" class="edit">
+    </li>
+</template>
+<script>
+export default {
+    name: 'Todo',
+    directives:{
+        focus(el, { value }, { context }){
+            if(value){
+                context.$nextTick(() => {
+                    el.focus()
+                })
+            }
+        }
+    },
+    props: {
+        todo: {
+            type: Object,
+            default: function(){
+                return {}
+            }
+        }
+    },
+    data(){
+        return {
+            editing: false
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/dashboard/admin/components/TodoList/index.vue
+++ src/views/dashboard/admin/components/TodoList/index.vue
@@ -1,11 +1,67 @@
 <template>
-    
+    <section class="todoapp">
+        <!-- header -->
+        <header class="header">
+            <input type="text" class="new-todo" autocomplete="off" placeholder="Todo List" >
+        </header>
+        <!-- main section -->
+        <section class="main">
+            <input  type="checkbox" id="toggle-all" class="toggle-all" >
+            <label for="toggle-all"></label>
+            <ul class="todo-list">
+                <todo 
+                 
+                />
+            </ul>
+        </section>
+        <!-- footer -->
+        <footer class="footer"> 
+            <span class="todo-count">
+                <strong>8</strong>
+                items left
+            </span> 
+            <ul class="filters">
+                <li>
+                    <a href="javascript:;">All</a>
+                </li>
+                <li>
+                    <a href="javascript:;">Active</a>
+                </li>
+                <li>
+                    <a href="javascript:;">Completed</a>
+                </li>
+            </ul>
+        </footer>
+    </section> 
 </template>
 <script>
+import Todo from './Todo.vue'
+
+const STORAGE_KEY = 'todos'
+const filters = {
+    all: todos => todos,
+    active: todos => todos.filter(todo => !todo.done),
+    completed: todos => todos.filter(todo => todo.done)
+}
+
+const defaultList = [
+  { text: 'star this repository', done: false },
+  { text: 'fork this repository', done: false },
+  { text: 'follow author', done: false },
+  { text: 'vue-element-admin', done: true },
+  { text: 'vue', done: true },
+  { text: 'element-ui', done: true },
+  { text: 'axios', done: true },
+  { text: 'webpack', done: true }
+]
+
 export default {
-    
+    name: 'TodoList',
+    components: {
+        Todo
+    }
 }
 </script>
 <style lang="scss" scoped>
-
+    @import './index.scss'
 </style>
