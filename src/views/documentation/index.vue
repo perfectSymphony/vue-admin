5891d73f61d41fd87ad5f57f030fca866a30470c
perfectSymphony <2066309334@qq.com>
2019-05-30 18:08:01 +0800

    文档页开发完成

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-container documentation-container">
-    <a class="document-btn" target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/">{{ $t('documentation.documentation') }}</a>
-    <a class="document-btn" target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">{{ $t('documentation.github') }}</a>
-    <a class="document-btn" target="_blank" href="https://panjiachen.gitee.io/vue-element-admin-site/zh/">国内文档</a>
-    <dropdown-menu :items="ProjectList" style="float:left;margin-left:50px;" title="系列项目" />
+    <a class="document-btn" target="_blank" href="https://github.com/perfectSymphony">{{ $t('documentation.documentation') }}</a>
+    <a class="document-btn" target="_blank" href="https://github.com/perfectSymphony/vue-admin">{{ $t('documentation.github') }}</a>
+    <a class="document-btn" target="_blank" href="https://panjiachen.gitee.io/vue-element-admin-site/zh/">博客园</a>
+    <dropdown-menu :items="ProjectList" style="float:left;margin-left:50px;" title="技术栈" />
   </div>
 </template>
 <script>
@@ -15,14 +15,15 @@ export default {
   data() {
     return {
       ProjectList: [
-        { title: '项目1', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目2', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目3', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目4', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目5', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目6', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目7', href: 'https://www.cnblogs.com/myprogramer/' },
-        { title: '项目8', href: 'https://www.cnblogs.com/myprogramer/' }
+        { title: 'HTML5', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: 'CSS3', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: 'JavaScript', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: 'NodeJS', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: 'Vue.js', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: 'electron', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: '性能优化篇', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: '数据结构和算法篇', href: 'https://www.cnblogs.com/myprogramer/' },
+        { title: '设计模式篇', href: 'https://www.cnblogs.com/myprogramer/' }
       ]
     }
   }
@@ -45,6 +46,5 @@ export default {
 	text-align: center;
 	foont-size: 20px;
   }
-
 }
 </style>
