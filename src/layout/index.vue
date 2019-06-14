e9b2216d6003bb57ec91c7b9d7eddd010bf7ce75
perfectSymphony <2066309334@qq.com>
2019-06-14 11:30:24 +0800

    '404'

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,59 +1,59 @@
 <template>
-    <div :class="classObj" class="app-wrapper">
-      <sidebar class="sidebar-container" />
-      <div class="main-container">
-        <navbar />  
-        <!-- 主体内容部分 -->
-        <tags-view /> 
-        <app-main />
-      </div>     
+  <div :class="classObj" class="app-wrapper">
+    <sidebar class="sidebar-container"/>
+    <div class="main-container">
+      <navbar/>
+      <!-- 主体内容部分 -->
+      <tags-view/>
+      <app-main/>
     </div>
+  </div>
 </template>
 <script>
-import { AppMain, Navbar, Sidebar, TagsView } from './components'
-import { mapState } from 'vuex'
+import { AppMain, Navbar, Sidebar, TagsView } from "./components";
+import { mapState } from "vuex";
 
-  export default {
-    name: 'Layout',
-    components: {
-      Navbar,
-      Sidebar,
-      AppMain,
-      TagsView
+export default {
+  name: "Layout",
+  components: {
+    Navbar,
+    Sidebar,
+    AppMain,
+    TagsView
+  },
+  computed: {
+    ...mapState({
+      sidebar: state => state.app.sidebar
+    }),
+    classObj() {
+      // console.log(this.sidebar)
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation
+      };
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
     },
-    computed: {
-      ...mapState({
-        sidebar: state => state.app.sidebar
-      }),
-      classObj() {
-        // console.log(this.sidebar)
-        return {
-          hideSidebar: !this.sidebar.opened,
-          openSidebar: this.sidebar.opened,
-          withoutAnimation: this.sidebar.withoutAnimation
-        }
-      }
-    },
-    methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      }
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
+};
 </script>
 <style lang="scss" scoped>
-   @import "~@/styles/mixin.scss";
-   @import "~@/styles/variables.scss";
-   .app-wrapper {
-       @include clearfix;
-       position: relative;
-       width: 100%;
-       height:100%;
-   }
-   .hideSidebar .fixed-header {
-     width: calc(100% - 54px)
-   }
+@import "~@/styles/mixin.scss";
+@import "~@/styles/variables.scss";
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.hideSidebar .fixed-header {
+  width: calc(100% - 54px);
+}
 </style>
