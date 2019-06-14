cfda99d9c3ec41221077b28ed70a0a41871a22a9
perfectSymphony <2066309334@qq.com>
2019-06-14 17:29:36 +0800

    '401未完'

--- src/views/error-page/401.vue
+++ src/views/error-page/401.vue
@@ -1,16 +1,81 @@
 <template>
-    <div>401</div>
+  <div class="errPage-container">
+    <el-button icon="arrow-left" class="back-btn" @click="back">返回</el-button>
+    <el-row>
+      <el-col :span="12">
+        <h1 class="text-jumbo text-ginormous">Oops!</h1>
+        <h2>你没有权限去该页面</h2>
+        <h6>如有不满意请联系你领导</h6>
+        <ul class="list-unstyled">
+          <li>或者你可以去:</li>
+          <li class="link-type">
+            <router-link to="/dashboard">返回首页</router-link>
+          </li>
+          <li>
+              <a href="#" @click.prevent="dialogVisible=true">点我看图</a>
+          </li>
+        </ul>
+      </el-col>
+      <el-col :span="12">
+        <img :src="errGif" width="313" height="428" alt="Girl has dropped her ice cream.">
+      </el-col>
+    </el-row>
+    <!-- vue 修饰符.sync的功能是：当一个子组件改变了一个 prop 的值时，这个变化也会同步到父组件中所绑定。 -->
+    <el-dialog :visible.sync="dialogVisible" title="随便看">
+        <img :src="ewizardClap" alt="img">
+    </el-dialog>
+  </div>
 </template>
 <script>
-export default {
-    name: 'Page401',
-    data(){
-        return{
+import errGif from "@/assets/401_images/401.gif";
 
-        }
+export default {
+  name: "Page401",
+  data() {
+    return {
+      errGif: errGif + "?" + +new Date(),
+      ewizardClap: 'https://wpimg.wallstcn.com/007ef517-bafd-4066-aae4-6883632d9646',
+      dialogVisible: false
     }
-}
+  },
+  methods: {
+      back() {
+          if(this.$route.query.noGoBack) {
+              this.$router.push({ path: '/dashboard' })
+          } else {
+              this.$router.go(-1)
+          }
+      }
+  }
+};
 </script>
 <style lang="scss" scoped>
-
+.errPage-container {
+  width: 800px;
+  max-width: 800px;
+  margin: 100px auto;
+  .back-btn {
+    background: #008489;
+    color: #fff;
+    border: none !important;
+  }
+  .text-jumbo {
+    font-size: 60px;
+    font-weight: 700;
+    color: #484848;
+  }
+  .list-unstyled {
+    font-size: 14px;
+    li {
+      padding-bottom: 5px;
+    }
+    a {
+      color: #008489;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
 </style>
--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -8,26 +8,26 @@
         <img class="pic-404__child right" src="@/assets/404_images/404_cloud.png" alt="404">
       </div>
       <div class="bullshit">
-          <div class="bullshit__oops">OOPS!</div>
-          <div class="bullshit__info">All rights reserved
-            <a style="color:#20a0ff" href="https://github.com/perfectSymphony/vue-admin" target="_blank">perfectSymphony</a>
-          </div>
-          <div class="bullshit__headline">{{ message }}</div>
-          <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-          <a href="" class="bullshit__return-home">Back to home</a>         
+         <div class="bullshit__oops">OOPS!</div>
+        <div class="bullshit__info">All rights reserved
+           <a style="color:#20a0ff" href="https://github.com/perfectSymphony/vue-admin" target="_blank">perfectSymphony</a>
+        </div>
+        <div class="bullshit__headline">{{ message }}</div>
+        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
+        <a href="" class="bullshit__return-home">Back to home</a>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-   name: "Page404",
-   computed: {
-       message(){
-           return 'The webmaster said that you can not enter this page...'
-       }
-   }
-};
+  name: 'Page404',
+  computed: {
+    message() {
+      return 'The webmaster said that you can not enter this page...'
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 .wscn-http404-container {
@@ -223,5 +223,4 @@ export default {
   }
 }
 </style>
-
 
