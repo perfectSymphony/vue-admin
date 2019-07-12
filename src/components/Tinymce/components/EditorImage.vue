e2e65dff7bd73709d8a5a3fd09f77435d39372db
perfectSymphony <2066309334@qq.com>
2019-07-12 18:02:36 +0800

    tinymce

--- src/components/Tinymce/components/EditorImage.vue
+++ src/components/Tinymce/components/EditorImage.vue
@@ -5,6 +5,12 @@
         </el-button>
         <el-dialog :visible.sync="dialogVisible">
             <el-upload
+               :multiple = "true"
+               :file-list = "fileList"
+               :show-file-list = "true"
+               :on-remove="handleRemove"
+               :on-success="handleSuccess"
+               :before-upload="beforeUpload"
                action="https://httpbin.org/post"
                class="editor-slide-upload"
                list-type="picture-card"
@@ -17,7 +23,7 @@
                 <el-button @click="dialogVisible = false">
                     Cancel
                 </el-button>
-                <el-button type="primary" @click="dialogVisible = false">
+                <el-button type="primary" @click="handleSubmit">
                     Confirm
                 </el-button>
             </span>
@@ -26,10 +32,68 @@
 </template>
 <script>
 export default {
-    name: 'EditorImage',
+    name: 'EditorSlideUpload',
     data(){
         return {
-            dialogVisible: false
+            dialogVisible: false,
+            listObj: {},
+            fileList: []
+        }
+    },
+    methods: {
+        checkAllSuccess(){
+            return Object.keys(this.listObj).every(item => this.listObj[item].hasSuccess)
+        },
+        handleSubmit(){
+            const arr = Object.keys(this.listObj).map(v => this.listObj[v])
+            if(!this.checkAllSuccess()){
+                this.$message('Please wait for all images to be uploaded successfully. If there is a network problem, please refresh the page and upload again!')
+                return
+            }
+            this.$emit('successCBK', arr)
+            this.listObj = {}
+            this.fileList = []
+            this.dialogVisible = false
+        },
+        handleSuccess(response, file){
+            const uid = file.uid
+            const objKeyArr = Object.keys(this.listObj)
+            for(let i = 0, len = objKeyArr.length; i < len; i++){
+                if(this.listObj[objKeyArr[i]].uid === uid){
+                    this.listObj[objKeyArr[i]].url = response.files.file
+                    this.listObj[objKeyArr[i]].hasSuccess = true
+                    return  
+                }
+            }
+        },
+        handleRemove(file){
+            const uid = file.uid
+            const objKeyArr = Object.keys(this.listObj)
+            for(let i = 0, len = objKeyArr.length; i < len; i++){
+                if(this.listObj[objKeyArr[i]].uid === uid){
+                    delete this.listObj[objKeyArr[i]]
+                    return 
+                }
+            }
+        },
+        beforeUpload(file){
+            const _this = this
+            const _URL = window.URL || window.webkitURL
+            const fileName = file.uid
+            this.listObj[fileName] = {}
+            return new Promise((resolve, reject) => {
+                const img = new Image()
+                img.src = _URL.createObjectURL(file)
+                img.onload = function(){
+                    _self.listObj[fileName] = {
+                        hasSuccess: false,
+                        uid: file.uid,
+                        width: this.width,
+                        height: this.height
+                    }
+                }
+                resolve(true)
+            })
         }
     }
 }
--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -2,7 +2,7 @@
     <div :class="{fullscreen: fullscreen}" class="tinymce-container" :style="{width: containerWidth}">
         <textarea :id="tinymceId" class="tinymce-textarea"/>
         <div class="editor-custom-btn-container">
-            <editorImage color="#1890ff" class="editor-upload-btn" />
+            <editorImage color="#1890ff" class="editor-upload-btn" @successCBK="imageSuccessCBK" />
         </div>
     </div>
 </template>
@@ -134,6 +134,12 @@ export default {
         },
         getContent(){
             window.tinymce.get(this.tinymceId).getContent()
+        },
+        imageSuccessCBK(arr){
+            const _this = this
+            arr.forEach(v => {
+                window.tinymce.get(_this.tinymceId).insertContent(`<img class="wscnph" src="${v.url}" >`)
+            })
         }
     }
 }
