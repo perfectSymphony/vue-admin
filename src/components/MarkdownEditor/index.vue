5ad863b091a6572d0e61b2aaf8e536ee4901c01e
perfectSymphony <2066309334@qq.com>
2019-07-17 18:15:11 +0800

    初始化

---
+++ src/components/MarkdownEditor/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+<script>
+//deps for editor
+import 'codemirror/lib/codemirror.css' // codemirror
+import 'tui-editor/dist/tui-editor.css' // editor ui
+import 'tui-editor/dist/tui-editor-contents.css' // editor content
+import 'highlight.js/styles/github.css' // code block highlight
+
+export default {
+    name: 'MarkdownEditor',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/components-demo/markdown.vue
+++ src/views/components-demo/markdown.vue
@@ -1,11 +1,27 @@
 <template>
-    <div class="app-container">
-        markdown
+    <div class="components-container">
+        <aside>Markdown is based on
+            <a href="https://github.com/nhnent/tui.editor" target="_blank">tui.editor</a> ，simply wrapped with Vue.
+            <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/feature/component/markdown-editor.html">
+                {{ $t('documentation.documentation') }}
+            </a>
+        </aside>
+        <div class="editor-container">
+            <el-tag class="tag-title">
+                Basic:
+            </el-tag>
+            <markdown-editor />
+        </div>
     </div>
 </template>
 <script>
+import MarkdownEditor from '@/components/MarkdownEditor'
+
 export default {
     name: 'markdown',
+    components: {
+        MarkdownEditor
+    },
     data(){
         return {
             
@@ -14,7 +30,12 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.editor-container {
+    margin-bottom: 30px;
+}
+.tag-title {
+    margin-bottom: 10px;
+}
 </style>
 
 
