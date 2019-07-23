710faae32781205cc36219139b0a03902f878dbe
perfectSymphony <2066309334@qq.com>
2019-07-23 17:06:23 +0800

    Markdown

--- src/views/components-demo/markdown.vue
+++ src/views/components-demo/markdown.vue
@@ -10,21 +10,76 @@
             <el-tag class="tag-title">
                 Basic:
             </el-tag>
-            <markdown-editor />
+            <markdown-editor v-model="content1" height="300px" />
         </div>
+        <div class="editor-container">
+            <el-tag class="tag-title">
+                Markdown Mode:
+            </el-tag>
+            <markdown-editor ref="markdownEditor" v-model="content2" :options= "{hideModeSwitch:true, previewStyle: 'tab'}" height="200px" />
+        </div>
+        <div class="editor-container">
+            <el-tag class="tag-title">
+                Customize Toolbar:
+            </el-tag>
+            <markdown-editor v-model="content3" :options="{ toolbarItems: ['heading','bold','italic']}" />
+        </div>
+        <div class="editor-container">
+            <el-tag class="tag-title">
+                I18n:
+            </el-tag>
+            <el-alert
+                :closable="false"
+                title="You can change the language of the admin system to see the effect"
+                type="success"
+            />
+            <markdown-editor ref="markdownEditor" v-model="content4" :language="language" height="300px" />
+        </div>
+        <el-button style="margin-top:80px;" type="primary" icon="el-icon-document" @click="getHtml">
+         Get HTML
+        </el-button>
+        <div v-html="html" />
     </div>
 </template>
 <script>
 import MarkdownEditor from '@/components/MarkdownEditor'
 
+const content = `
+**This is test**
+
+* vue
+* element
+* webpack
+
+`
+
 export default {
-    name: 'markdown',
+    name: 'MarkdownDemo',
     components: {
         MarkdownEditor
     },
     data(){
         return {
-            
+            content1: content,
+            content2: content,
+            content3: content,
+            content4: content,
+            html: '',
+            languageTypeList: {
+                'en': 'en_US',
+                'zh': 'zh_CN',
+                'es': 'es_ES'
+            } 
+        }
+    },
+    computed: {
+        language(){
+            return this.languageTypeList[this.$store.getters.language]
+        }
+    },
+    methods: {
+        getHtml(){
+            this.html = this.$refs.markdownEditor.getHtml()
         }
     }
 }
