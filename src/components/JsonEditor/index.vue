63078fe94725792443613890ecb32fa2f850a7a4
perfectSymphony <2066309334@qq.com>
2019-07-23 17:48:25 +0800

    调用CodeMirror插件

---
+++ src/components/JsonEditor/index.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: '',
+        data(){
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
--- src/views/components-demo/json-editor.vue
+++ src/views/components-demo/json-editor.vue
@@ -1,19 +1,32 @@
 <template>
-    <div class="components-container">
-        json-editor
-    </div>
+  <div class="components-container">
+    <aside>
+      Json-Editor is base on
+      <a
+        href="https://github.com/codemirror/CodeMirror"
+        target="_blank"
+      >CodeMirrorr</a>. Lint
+      base on
+      <a
+        href="https://github.com/codemirror/CodeMirror/blob/master/addon/lint/json-lint.js"
+        target="_blank"
+      >json-lint</a>.
+    </aside>
+    <div class="editor-container">3</div>
+  </div>
 </template>
 <script>
-export default {
-    name: 'jsonEditor',
-    data(){
-        return {
+import JsonEditor from '@/components/JsonEditor'
 
-        }
-    }
-}
+const jsonData ='[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"CORN"}],"name":""},{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"XAGUSD"},{"market_type":"forexdata","symbol":"AUTD"},{"market_type":"forexdata","symbol":"AGTD"}],"name":"贵金属"},{"items":[{"market_type":"forexdata","symbol":"CORN"},{"market_type":"forexdata","symbol":"WHEAT"},{"market_type":"forexdata","symbol":"SOYBEAN"},{"market_type":"forexdata","symbol":"SUGAR"}],"name":"农产品"},{"items":[{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"USOIL"},{"market_type":"forexdata","symbol":"NGAS"}],"name":"能源化工"}]';
+
+export default {
+  name: "jsonEditorDemo",
+  data() {
+    return {};
+  }
+};
 </script>
 <style lang="scss" scoped>
-
 </style>
 
