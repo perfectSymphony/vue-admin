1730b77553728be2dfb92700b91bc8631d3cebb2
perfectSymphony <2066309334@qq.com>
2019-07-29 18:08:45 +0800

    avatar-upload

---
+++ src/components/ImageCropper/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+<script>
+export default {
+    name: 'ImageCropperDemo',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/components-demo/avatar-upload.vue
+++ src/views/components-demo/avatar-upload.vue
@@ -1,14 +1,30 @@
 <template>
     <div class="components-container">
-123
+        <liu-thumb :image="image" />
+        <el-button type="primary" icon="upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">
+        Change Avatar
+        </el-button>
+        <image-cropper
+        v-show="imagecropperShow"
+        >
+
+        </image-cropper>
     </div>
 </template>
 <script>
+import ImageCropper from '@/components/ImageCropper'
+import liuThumb from '@/components/liuThumb'
+
 export default {
     name: 'avatar-upload',
+    components: {
+        ImageCropper,
+        liuThumb
+    },
     data(){
         return {
-
+            imagecropperShow: false,
+            image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
         }
     }
 }
