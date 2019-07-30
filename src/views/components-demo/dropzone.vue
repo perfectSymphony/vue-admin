2622a24ebfdd8a1fab48e48672d55b125da93fce
perfectSymphony <2066309334@qq.com>
2019-07-30 14:36:07 +0800

    初始化图片上传

---
+++ src/views/components-demo/dropzone.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="components-container">
+        123
+    </div>
+</template>
+<script>
+export default {
+    name: 'Dropzone',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
