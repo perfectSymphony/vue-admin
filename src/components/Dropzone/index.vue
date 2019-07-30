578d2906e9282005edd20652b08deb9714534096
perfectSymphony <2066309334@qq.com>
2019-07-30 18:15:57 +0800

    自定义图片上传开发中。。。

---
+++ src/components/Dropzone/index.vue
@@ -0,0 +1,105 @@
+<template>
+    <div :id="id" :ref="id" :action="url" class="dropzone">
+        <input type="file" name="file">
+    </div>
+</template>
+<script>
+
+import Dropzone from 'dropzone'
+import 'dropzone/dist/dropzone.css'
+
+// 禁止对所有元素的自动查找:
+Dropzone.autoDiscover = false;
+
+export default {
+    name: 'Dropzone',
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        },
+        clickable: {
+            type: Boolean,
+            default: true
+        },
+        defaultMsg: {
+            type: String,
+            default: '上传图片'
+        },
+        acceptedFiles: {
+            type: String,
+            default: ''
+        },
+        thumbnailHeight: {
+            type: Number,
+            default: 200
+        },
+        thumbnailWidth: {
+            type: Number,
+            default: 200
+        },
+        showRemoveLink: {
+            type: Boolean,
+            default: true
+        },
+        maxFilesize: {
+            type: Number,
+            default: 2
+        },
+        maxFiles: {
+            type: Number,
+            default: 3
+        },
+        autoProcessQueue: {
+            type: Boolean,
+            default: true
+        },
+        useCustomDropzoneOptions: {
+            type: Boolean,
+            default: false
+        },
+        defaultImg: {
+            type: [String, Array],
+            default: ''
+        },
+        couldPaste: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data(){
+        return {
+            dropzone: ''
+        }
+    },
+    mounted(){
+        const element =document.getElementById(this.id)
+        const vm = this 
+        this.dropzone = new Dropzone(element, {
+            clickable: this.clickable,
+            thumbnailWidth: this.thumbnailWidth,
+            thumbnailHeight: this.thumbnailHeight,
+            maxFiles: this.maxFiles,
+            maxFilesize: this.maxFilesize,
+            dictRemoveFile: 'Remove',
+            addRemoveLinks: this.showRemoveLink,
+            acceptedFiles: this.acceptedFiles,
+            autoProcessQueue: this.autoProcessQueue,
+            dictDefaultMessage: '<i style="margin-top: 3em;display: inline-block" class="material-icons">' + this.defaultMsg + '</i><br>Drop files here to upload',
+            dictMaxFilesExceeded: '最多是三张图片',
+            previewTemplate: '<div class="dz-preview dz-file-preview">  <div class="dz-image" style="width:' + this.thumbnailWidth + 'px;height:' + this.thumbnailHeight + 'px" ><img style="width:' + this.thumbnailWidth + 'px;height:' + this.thumbnailHeight + 'px" data-dz-thumbnail /></div>  <div class="dz-details"><div class="dz-size"><span data-dz-size></span></div> <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>  <div class="dz-error-message"><span data-dz-errormessage></span></div>  <div class="dz-success-mark"> <i class="material-icons">done</i> </div>  <div class="dz-error-mark"><i class="material-icons">error</i></div></div>',
+            
+            // 事件监听器只能在Dropzone的instances[实例] 上注册 , 添加你的事件监听器最好的地方是 在 init 方法中
+            init(){},
+        })
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
--- src/views/components-demo/dropzone.vue
+++ src/views/components-demo/dropzone.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="components-container">
-        123
+        <aside>自己封装一个图片上传的插件</aside>
+        <dropzone id="myVueDropzone" url="https://httpbin.org/post"  />
     </div>
 </template>
 <script>
+
+import Dropzone from '@/components/Dropzone'
+
 export default {
-    name: 'Dropzone',
+    name: 'DropzoneDemo',
+    components: {
+        Dropzone
+    },
     data(){
         return {
 
