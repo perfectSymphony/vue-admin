d26c48faac4d36205dcb168f0e97369542f03c0d
perfectSymphony <2066309334@qq.com>
2019-07-30 14:18:11 +0800

    头像上传功能-参考官网提供的示例

--- src/views/components-demo/avatar-upload.vue
+++ src/views/components-demo/avatar-upload.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="components-container">
-        <liu-thumb :image="image" />
-        <el-button type="primary" icon="upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">
+        <liu-thumb :image="imgDataUrl" />
+
+        <el-button type="primary" icon="upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="toggleShow">
         Change Avatar
         </el-button>
-        <image-cropper
-        v-show="imagecropperShow"
-        >
-
-        </image-cropper>
+        <image-cropper field="img"
+            @crop-success="cropSuccess"
+            @crop-upload-success="cropUploadSuccess"
+            @crop-upload-fail="cropUploadFail"
+            v-model="show"
+            :width="300"
+            :height="300"
+            url="https://httpbin.org/post"
+            :no-rotate="false"
+            :params="params"
+            :headers="headers"
+            img-format="png"></image-cropper>
     </div>
 </template>
 <script>
+import 'babel-polyfill'
 import ImageCropper from '@/components/ImageCropper'
 import liuThumb from '@/components/liuThumb'
 
@@ -23,8 +32,35 @@ export default {
     },
     data(){
         return {
-            imagecropperShow: false,
-            image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
+            imgDataUrl: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191',
+            show: false,
+            // 传附带其他数据，格式"{k:v}"
+			params: {
+				token: '123456798',
+				name: 'avatar'
+			},
+			headers: {
+				smail: '*_~'
+			}
+        }
+    },
+    methods: {
+        toggleShow() {
+            this.show = !this.show;
+        },
+        cropSuccess(imgDataUrl, field){
+            console.log('-------- crop success --------');
+            this.imgDataUrl = imgDataUrl;
+        },
+        cropUploadSuccess(jsonData, field){
+            console.log('-------- upload success --------');
+            console.log(jsonData);
+            console.log('field: ' + field);
+        },
+        cropUploadFail(status, field){
+            console.log('-------- upload fail --------');
+            console.log(status);
+            console.log('field: ' + field);
         }
     }
 }
