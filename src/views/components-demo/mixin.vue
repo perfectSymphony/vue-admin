5fff5a5a38fcf892a44dd03ef4039cf8829285d4
perfectSymphony <2066309334@qq.com>
2019-08-06 18:13:59 +0800

    小组件开发中...

--- src/views/components-demo/mixin.vue
+++ src/views/components-demo/mixin.vue
@@ -1,6 +1,22 @@
 <template>
-    <div class="components-container">
-        mixin
+    <div class="mixin-components-container">
+        <el-row>
+            <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <span>按钮</span>
+            </div>
+            <div style="margin-bottom: 50px;">
+                <el-col :span="4" class="text-center">
+                    <router-link class="liu-btn blue-btn" to="/documentation/index">
+                    Documentation
+                    </router-link>                    
+                </el-col>
+                <el-col :span="4" class="text-center"><div class="grid-content bg-purple">2</div></el-col>
+                <el-col :span="4" class="text-center"><div class="grid-content bg-purple">3</div></el-col>
+                <el-col :span="4" class="text-center"><div class="grid-content bg-purple">4</div></el-col>
+            </div>
+            </el-card>   
+        </el-row>
     </div>
 </template>
 
@@ -16,5 +32,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.mixin-components-container {
+    background-color: #f0f2f5;
+    padding: 30px;
+    min-height: calc(100vh - 84px);
+}
 
 </style>
