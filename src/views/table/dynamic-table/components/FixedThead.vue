5b4194b3b0ff106f84269cb3a6d9dd4832d72c0e
perfectSymphony <2066309334@qq.com>
2019-08-20 15:26:59 +0800

    完成动态表格加载模块

--- src/views/table/dynamic-table/components/FixedThead.vue
+++ src/views/table/dynamic-table/components/FixedThead.vue
@@ -1,60 +1,60 @@
 <template>
-    <div class="app-container">
-        <div class="filter-container">
-            <el-checkbox-group v-model="checkedCities">
-                <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-            </el-checkbox-group>
-        </div>
-
-        <el-table :key="key" :data="tableData" border fit highlight-current-row style="width: 100%">
-        <el-table-column prop="name" label="城市名称" width="180" />
-        <el-table-column v-for="city in formThead" :key="city" :label="city">
-            <template slot-scope="scope">
-            {{ scope.row[city] }}
-            </template>
-        </el-table-column>
-        </el-table>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-checkbox-group v-model="checkboxVal">
+        <el-checkbox v-for="fruit in fruits" :key="fruit" :label="fruit">
+          {{fruit}}
+        </el-checkbox>
+      </el-checkbox-group>
     </div>
+
+    <el-table :key="key" :data="tableData" border fit highlight-current-row style="width: 100%">
+      <el-table-column prop="水果" label="fruitName" width="180" />
+      <el-table-column v-for="fruit in formThead" :key="fruit" :label="fruit">
+        <template slot-scope="scope">
+          {{ scope.row[fruit] }}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 
-  const cityOptions = ['上海', '北京', '广州', '深圳']
+const fruitOptions = ['苹果', '香蕉', '橘子', '番茄']
+const defaultFormThead = ['苹果', '香蕉']
 
-  const checkedCities = ['上海', '北京']
-
-  export default {
-    data() {
-      return {
-        tableData: [
-            {
-            name: '城市名称-1',
-            SH: '上海-10',
-            BJ: '北京-10',
-            GZ: '广州-10'
-            },
-            {
-            name: '城市名称-2',
-            SH: '上海-20',
-            BJ: '北京-20',
-            GZ: '广州-20'
-            }
-        ],
-        key: 1,
-        checkAll: false,
-        checkedCities: checkedCities,
-        formThead: checkedCities,
-        cities: cityOptions,
-      }
-    },
-    watch: {
-        checkedCities(valArr){
-            console.log(valArr)
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          水果: '水果',
+          苹果: '苹果',
+          香蕉: '香蕉',
+          橘子: '橘子',
+          番茄: '番茄'
+        },
+        {
+          水果: '水果',
+          苹果: '苹果',
+          香蕉: '香蕉',
+          橘子: '橘子',
+          番茄: '番茄'
         }
+      ],
+      key: 1, // table key
+      fruits: fruitOptions,
+      checkboxVal: defaultFormThead, // checkboxVal
+      formThead: defaultFormThead // 默认表头 Default header
+    }
+  },
+  watch: {
+    checkboxVal(valArr) {
+      this.formThead = valArr
+      this.key = this.key + 1// 为了保证table 每次都会重渲 
     }
   }
+}
 </script>
 
-<style>
-
-</style>
--- src/views/table/dynamic-table/components/UnfixedThead.vue
+++ src/views/table/dynamic-table/components/UnfixedThead.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-      unfixed
+  <div class="app-container">
+    <div class="filter-container">
+      <el-checkbox-group v-model="formThead">
+        <el-checkbox v-for="fruit in fruits" :key="fruit" :label="fruit">
+          {{fruit}}
+        </el-checkbox>
+      </el-checkbox-group>
+    </div>
+
+    <el-table :data="tableData" border fit highlight-current-row style="width: 100%">
+      <el-table-column prop="name" label="fruitName" width="180" />
+      <el-table-column v-for="fruit in formThead" :key="fruit" :label="fruit">
+        <template slot-scope="scope">
+          {{ scope.row[fruit] }}
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-export default {
 
+const fruitOptions = ['apple', 'banana', 'orange', 'tomato']
+const defaultFormThead = ['apple', 'banana']
+
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          name: 'fruit-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10',
+          tomato: 'tomato-10'
+        },
+        {
+          name: 'fruit-2',
+          apple: 'apple-20',
+          banana: 'banana-20',
+          orange: 'orange-20',
+          tomato: 'tomato-20'
+        }
+      ],
+      fruits: fruitOptions,
+      formThead: defaultFormThead // 默认表头 Default header
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
