5ff7c9c28fb76234755ac028544bbad90405b0d5
perfectSymphony <2066309334@qq.com>
2019-09-06 00:21:36 +0800

    开发主题颜色拾取器

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-color-picker v-model="color" 
+    <el-color-picker v-model="theme" 
        popper-class="theme-picker-dropdown"
        :predefine = 'predefineColors'
        class="theme-picker"
@@ -11,11 +11,13 @@
 const version = require('element-ui/package.json').version  //从node_modules中获取element-ui的版本号
 const ORIGINAL_THEME = '#409EFF'  //默认颜色
 
+// 该回调将会在侦听开始之后被立即调用
 export default {
     name: 'ThemePicker',
     data(){
         return {
-            color: '',
+            theme: '',
+            chalk: '',
             predefineColors: [
                 '#409EFF', 
                 '#1890ff', 
@@ -26,6 +28,107 @@ export default {
                 '#6959CD', 
                 '#f5222d'
             ]
+        }
+    },
+    computed: {
+        defaultTheme(){
+            return this.$tore.state.settings.theme
+        }
+    },
+    watch: {
+        defaultTheme: {
+            handler: function(val, oldVal){
+                this.theme = val
+            },
+            immediate: true
+        },
+        async theme(val){
+            const oldVal = this.chalk ? this.theme : ORIGINAL_THEME
+            if(typeof val !== 'string') return 
+            const themeCluster = this.getThemeCluster(val.replace('#', ''))
+            const originalCluster = this.getThemeCluster(oldVal.replace('#', ''))
+            console.log(themeCluster, originalCluster)
+
+            const $message = this.$message({
+                message: 'Compiling the theme',
+                customClass: 'theme-message',
+                type: 'success',
+                duration: 10,
+                iconClass: 'el-icon-loading'
+            })
+
+            const getHandler = (variable, id) => {
+                return () => {
+                    const originalCluster = this.getThemeCluster(ORIGINAL_THEME.replace('#', ''))
+                    const newStyle = this.updateStyle(this[variable], originalCluster, themeCluster)
+
+                    let styleTag = document.getElementById(id)
+                    if(!styleTag){
+                        styleTag = document.createElement('style')
+                        styleTag.setAttribute('id', id)
+
+                        document.head.appendChild(styleTag)
+                    }
+                    styleTag.innerText = newStyle
+                }
+            }
+
+            if(!this.chalk){
+                const url = `https://unpkg.com/element-ui@${version}/lib/theme-chalk/index.css`
+                await this.getCSSString(url, 'chalk')
+            }
+
+            const chalkHandler = getHandler('chalk', 'chalk-style')
+            chalkHandler()
+
+            const styles = [].slice.call(document.querySelectorAll('style'))
+                .filter(style => {
+                    const text = style.innerText
+                    return new RegExp(oldVal, 'i').test(text) && !/Chalk Variables/.test(text)
+                })
+        }
+    },
+    methods: {
+        getThemeCluster(theme){
+            const tintColor = (color, tint) => {
+                let red = parseInt(color.slice(0, 2), 16)
+                let green = parseInt(color.slice(2, 4), 16)
+                let blue = parseInt(color.slice(4, 6), 16)
+
+                if(tint === 0){  // when primary color is in its rgb space
+                    return [red, green, blue].join(',')
+                } else {
+                    red += Math.round(tint * (255 - red))
+                    green += Math.round(tint * (255 - green))
+                    blue += Math.round(tint * (255 - blue))
+
+                    red = red.toString(16)
+                    green = green.toString(16)
+                    blue = blue.toString(16)
+
+                    return `#${red}${green}${blue}`
+                }
+            }
+        },
+        updateStyle(style, oldCluster, newCluster){
+            let newStyle = style
+            oldCluster.forEach((color, index) => {
+                newStyle = newStyle.replace(new RegExp(color, 'ig'), newCluster[index])
+            })
+            return newStyle
+        },
+        getCSSString(url, variable){
+            return new Promise(resolve => {
+                const xhr = new XMLHttpRequest()
+                xhr.onreadystatechange = () => {
+                    if(xhr.readyState === 4 && xhr.status === 200){
+                        this[variable] = xhr.responseText.replace(/@font-face{[^}]+}/, '')
+                        resolve()
+                    }
+                }
+                xhr.open('GET', url)
+                xhr.send()
+            })
         }
     }
 }
